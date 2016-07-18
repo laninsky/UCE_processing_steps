102ca1e34a7fea9ad05b4afbe5e922a709ca0476
Alana Alexander <laninsky@gmail.com>
2016-07-18 15:20:33 -0500

    Update filtering_overrepresented.R

--- filtering_overrepresented.R
+++ filtering_overrepresented.R
@@ -1,3 +1,4 @@
+library(stringi)
 forwards <- list.files()[grep("R1",list.files())]
 reverses <- list.files()[grep("R2",list.files())]
 overrepresented <- as.matrix(read.table("/home/a499a400/manu/combined/overrepresented_sequences.fas"))
@@ -15,18 +16,62 @@ outputrecord[(i+1),3] <- length(fastalines)
 
 blankboth <- which(((temp1[fastalines]=="") & (temp2[fastalines]==""))==TRUE)
 outputrecord[(i+1),4] <- length(blankboth)
+
+if (length(blankboth)>0) {
 blankboth <- sort(c(fastalines[blankboth],fastalines[blankboth]+1,fastalines[blankboth]+2,fastalines[blankboth]-1))
 temp1 <- temp1[-blankboth]
 temp2 <- temp2[-blankboth]
+}
 
+for (j in seq(2,(dim(overrepresented)[1]),2)) {
+revcomp <- stri_reverse(chartr("acgtACGT","tgcaTGCA",overrepresented[j,1]))
 
-blankreverses <- which((temp2[fastalines]=="")==TRUE)
+tempcount <- which(((grepl(overrepresented[j,1],temp1[fastalines])) | (grepl(overrepresented[j,1],temp2[fastalines])) | (grepl(revcomp,temp1[fastalines])) | (grepl(revcomp,temp2[fastalines])))==TRUE)
 
+outputrecord[(i+1),((j/2)+6)] <- length(tempcount)
 
+if (length(tempcount)>0) {
+tempcount <- sort(c(fastalines[tempcount],fastalines[tempcount]+1,fastalines[tempcount]+2,fastalines[tempcount]-1))
 
+temp1 <- temp1[-tempcount]
+temp2 <- temp2[-tempcount]
+}
+}
 
-temp1[fastalines[blankforwards]]
+blankforward <- which((temp1[fastalines]=="")==TRUE)
+outputrecord[(i+1),5] <- length(blankforward)
 
-which(grepl(temp1[fastalines],"")==TRUE)
+if (length(blankforward)>0) {
+blankforward <- sort(c(fastalines[blankforward],fastalines[blankforward]+1,fastalines[blankforward]+2,fastalines[blankforward]-1))
+newname <- gsub("R1","singleton",forwards[i])
+newname <- gsub(".gz","",newname)
+write.table(temp2[blankforward],newname,quote=FALSE,col.names=FALSE,row.names=FALSE)
 
-singletonout <- 
+temp1 <- temp1[-blankforward]
+temp2 <- temp2[-blankforward]
+}
+
+blankreverse <- which((temp2[fastalines]=="")==TRUE)
+outputrecord[(i+1),6] <- length(blankreverse)
+
+if (length(blankreverse)>0) {
+blankreverse <- sort(c(fastalines[blankreverse],fastalines[blankreverse]+1,fastalines[blankreverse]+2,fastalines[blankreverse]-1))
+newname <- gsub("R1","singleton",forwards[i])
+newname <- gsub(".gz","",newname)
+write.table(temp1[blankreverse],newname,quote=FALSE,append=TRUE,col.names=FALSE,row.names=FALSE)
+
+temp1 <- temp1[-blankreverse]
+temp2 <- temp2[-blankreverse]
+}
+
+newFname <- gsub("R1","R1_repeatremoved",forwards[i])
+newFname <- gsub(".gz","",newFname)
+newRname <- gsub("R2","R2_repeatremoved",reverse[i])
+newRname <- gsub(".gz","",newRname)
+
+write.table(temp1,newFname,quote=FALSE,col.names=FALSE,row.names=FALSE)
+write.table(temp2,newRname,quote=FALSE,col.names=FALSE,row.names=FALSE)
+
+}
+
+write.table(outputrecord,"outputrecord.txt",quote=FALSE,col.names=FALSE,row.names=FALSE)
