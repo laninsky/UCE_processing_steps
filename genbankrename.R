b2a730fbd57b3539d850d86a83903d3ac6c2b699
Alana Alexander <laninsky@gmail.com>
2016-11-10 11:03:50 -0600

    Create genbankrename.R

---
+++ genbankrename.R
@@ -0,0 +1,35 @@
+library(stringr)
+
+key <- as.matrix(read.table("key",sep="\t",quote=""))
+
+files <- list.files(pattern="*.fasta")
+
+outputsequence <- NULL
+
+for (i in files) {
+tempfile <- as.matrix(read.table(i,sep="\t"))
+ucelocus <- gsub(".fasta","",i)
+
+for (j in 1:dim(key)[1]) {
+tempfile[(which(tempfile[,1]==key[j,1])),1] <- eval(parse(text=key[j,2]))
+}
+
+linestoditch <- matrix(FALSE,ncol=1,nrow=(dim(tempfile)[1]))
+
+for (j in seq(2, dim(tempfile)[1],2)) {
+linestoditch[j,1] <- nchar(gsub("-","",tempfile[j,1]))==0
+if (linestoditch[j,1]==TRUE) {
+linestoditch[(j-1),1] <- TRUE
+}
+}
+
+removingmissing <- as.matrix(tempfile[(which(linestoditch[,1]==FALSE)),],ncol=1)
+
+for (j in seq(2, dim(removingmissing)[1],2)) {
+removingmissing[j,1] <- gsub("-","",removingmissing[j,1])
+}
+
+outputsequence <- rbind(outputsequence,removingmissing)
+}
+
+write.table(outputsequence,"ucegenbanksequence.fasta",quote=FALSE,row.names=FALSE,col.names=FALSE)
