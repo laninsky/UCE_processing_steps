dade5a3bdd089c44aa102ce2ec5cef3215562ca3
Alana Alexander <laninsky@gmail.com>
2017-03-01 10:34:37 -0600

    Update missing_data.R

--- missing_data.R
+++ missing_data.R
@@ -10,7 +10,7 @@ for (i in 1:(dim(data)[1])) {
     output <- rbind(output,(unlist(strsplit(data[i,2],""))))
 }
 
-missing_by_sample <- c("samples","mean")
+missing_by_sample <- c("samples","mean_%_missing")
 for (i in 1:(dim(data)[1])) {
    tempmissing <- c(data[i,1],((sum(output[i,1:(dim(output)[2])]=="?")/(dim(output)[2]))*100))
    missing_by_sample <- rbind(missing_by_sample,tempmissing)
@@ -24,7 +24,7 @@ for (i in 1:(dim(output)[2])) {
     nomissing <- c(nomissing,sum(output[1:(dim(output)[1]),i]=="?"))
 }
     
-output <- c("number of missing samples","number of sites")
+output <- c("number_of_missing_samples","number_of_sites")
 for (i in 0:(dim(data)[1])) {
     tempoutput <- c(i,sum(nomissing==i))
     output <- rbind(output,tempoutput)
