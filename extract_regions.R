b69956b6c703fb654660d8b028714e896d3e8a5f
Alana Alexander <laninsky@gmail.com>
2017-06-04 20:20:37 -0500

    Update extract_regions.R

--- extract_regions.R
+++ extract_regions.R
@@ -5,3 +5,19 @@ fasta_suffix <- ".fasta"
 feature_suffix <- "_feature_table.txt"
 
 setwd(working_dir)
+
+for (i in list.files(pattern=fasta_suffix)) {
+    temp_fasta <-  read.table(i,header=FALSE,stringsAsFactors=FALSE,sep="\t")
+    fasta <- temp_fasta[1,1]
+    sequencepaste <- NULL
+    for (j in 2:(dim(temp_fasta)[1])) {
+      if ((length(grep(">",temp_fasta[j,1])))>0) {
+      to_write <- toupper(sequencepaste)
+      fasta <- rbind(fasta,to_write)
+      fasta <- rbind(fasta,temp_fasta[j,1])
+      sequencepaste <- NULL
+      } else {
+      sequencepaste <- paste(sequencepaste,temp_fasta[j,1],sep="")
+      }
+    }
+    fasta <- rbind(fasta,toupper(sequencepaste))
