b11ed4b9b2214831be99174056e14c1453ce07dd
Alana Alexander <laninsky@gmail.com>
2017-06-04 19:30:33 -0500

    Create extract_regions.R

---
+++ extract_regions.R
@@ -0,0 +1,7 @@
+# 
+
+working_dir <- "C:\\Users\\Alana\\Dropbox\\beetles\\mitogenomes_18S_28S\\mitogenome\\final"
+fasta_suffix <- ".fasta"
+feature_suffix <- "_feature_table.txt"
+
+setwd(working_dir)
